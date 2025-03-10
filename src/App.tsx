f94cc5ba64b7f65a76c940a1ebc0cbb9ed6e7829
Felipe Marcon <felipeemarcon@gmail.com>
2025-03-10 18:39:27 -0300

    feat: update jade

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { CardContainer, GroupHeader, ListItem, Text } from '@stone-payments/jade';
-import { useState } from 'react';
+import { CardContainer, GroupHeader, JadeProvider, ListItem, Text } from '@stone-payments/jade';
+import { useRef, useState } from 'react';
 import Container from './components/container/container';
 import Grid from './components/grid/grid';
 import Header from './components/header/header';
@@ -9,77 +9,82 @@ import SampleTable from './parts/table';
 import { ContainerVariant } from './types';
 
 export default function App() {
+  const portalRef = useRef<HTMLDivElement>(null);
+
   const [isSidebarVisible, setIsSidebarVisible] = useState<boolean>(true);
   const [containerVariant, setContainerVariant] = useState<ContainerVariant>('default');
 
   return (
-    <div
-      className={`jade stone flex flex-col content-start md:flex-row ${isSidebarVisible ? 'md:pl-[80px] lg:pl-0' : ''}`}
-    >
-      <Sidebar visibility={isSidebarVisible} />
+    <JadeProvider theme="stone" portalRoot={portalRef}>
       <div
-        className={`relative ml-0 flex w-full flex-col ${isSidebarVisible ? 'lg:ml-(--app-sidebar-width-lg)' : ''} transition-all duration-500 ease-in-out`}
+        className={`jade stone flex flex-col content-start md:flex-row ${isSidebarVisible ? 'md:pl-[80px] lg:pl-0' : ''}`}
       >
-        <Header containerVariant={containerVariant} />
-        <Container
-          variant={containerVariant}
-          className={`@container lg:pb-24`}
-          data-name="main-content"
+        <Sidebar visibility={isSidebarVisible} />
+        <div
+          className={`relative ml-0 flex w-full flex-col ${isSidebarVisible ? 'lg:ml-(--app-sidebar-width-lg)' : ''} transition-all duration-500 ease-in-out`}
         >
-          <Grid className="items-start">
-            <div className="col-span-full">
-              <CardContainer variant="outlined">
-                <SampleTable />
-              </CardContainer>
-            </div>
-            <main className="col-span-full flex flex-col items-start gap-4 lg:col-span-5 lg:gap-5 xl:col-span-8 @lg:@max-xl:col-span-full @lg:@max-xl:flex-row @8xl:col-span-12">
-              <CardContainer className="w-full" data-name="details">
-                <GroupHeader title="Detalhes" />
-                {Array.from({ length: 5 }).map((_, index) => (
-                  <ListItem
-                    key={index}
-                    content={<Text color="medium">Label</Text>}
-                    trailing={<Text weight="semibold">Value</Text>}
-                  />
-                ))}
-              </CardContainer>
-              <CardContainer className="w-full" data-name="other-infos">
-                <GroupHeader title="Outras infos" />
-                {Array.from({ length: 5 }).map((_, index) => (
-                  <ListItem
-                    key={index}
-                    content={<Text color="medium">Label</Text>}
-                    trailing={<Text weight="semibold">Value</Text>}
-                  />
-                ))}
-              </CardContainer>
-            </main>
-            <CardContainer
-              as="aside"
-              className="col-span-full lg:col-span-3 xl:col-span-4 @lg:@max-xl:col-span-full"
-              data-name="sidebar"
-            >
-              <GroupHeader title="Sidebar" />
-              <div className="py-1">
-                {Array.from({ length: 8 }).map((_, index) => (
-                  <ListItem
-                    key={index}
-                    content={<ListItem.Content label="Label" description="Description" />}
-                    onClick={() => console.log(index)}
-                  />
-                ))}
+          <Header containerVariant={containerVariant} />
+          <Container
+            variant={containerVariant}
+            className={`@container lg:pb-24`}
+            data-name="main-content"
+          >
+            <Grid className="items-start">
+              <div className="col-span-full">
+                <CardContainer variant="outlined">
+                  <SampleTable />
+                </CardContainer>
               </div>
-            </CardContainer>
-          </Grid>
-        </Container>
+              <main className="col-span-full flex flex-col items-start gap-4 lg:col-span-5 lg:gap-5 xl:col-span-8 @lg:@max-xl:col-span-full @lg:@max-xl:flex-row @8xl:col-span-12">
+                <CardContainer className="w-full" data-name="details">
+                  <GroupHeader title="Detalhes" />
+                  {Array.from({ length: 5 }).map((_, index) => (
+                    <ListItem
+                      key={index}
+                      content={<Text color="medium">Label</Text>}
+                      trailing={<Text weight="semibold">Value</Text>}
+                    />
+                  ))}
+                </CardContainer>
+                <CardContainer className="w-full" data-name="other-infos">
+                  <GroupHeader title="Outras infos" />
+                  {Array.from({ length: 5 }).map((_, index) => (
+                    <ListItem
+                      key={index}
+                      content={<Text color="medium">Label</Text>}
+                      trailing={<Text weight="semibold">Value</Text>}
+                    />
+                  ))}
+                </CardContainer>
+              </main>
+              <CardContainer
+                as="aside"
+                className="col-span-full lg:col-span-3 xl:col-span-4 @lg:@max-xl:col-span-full"
+                data-name="sidebar"
+              >
+                <GroupHeader title="Sidebar" />
+                <div className="py-1">
+                  {Array.from({ length: 8 }).map((_, index) => (
+                    <ListItem
+                      key={index}
+                      content={<ListItem.Content label="Label" description="Description" />}
+                      onClick={() => console.log(index)}
+                    />
+                  ))}
+                </div>
+              </CardContainer>
+            </Grid>
+          </Container>
 
-        <LayoutControls
-          containerVariant={containerVariant}
-          sidebarVisiblity={isSidebarVisible}
-          onContainerChange={setContainerVariant}
-          onSidebarVisibilityChange={setIsSidebarVisible}
-        />
+          <LayoutControls
+            containerVariant={containerVariant}
+            sidebarVisiblity={isSidebarVisible}
+            onContainerChange={setContainerVariant}
+            onSidebarVisibilityChange={setIsSidebarVisible}
+          />
+        </div>
       </div>
-    </div>
+      <div id="jade-portal-root" ref={portalRef} />
+    </JadeProvider>
   );
 }
--- src/components/layout-controls/layout-controls.tsx
+++ src/components/layout-controls/layout-controls.tsx
@@ -1,4 +1,12 @@
-import { ActionButton, DropdownMenu, DropdownMenuItem, Switch, Text } from '@stone-payments/jade';
+import {
+  ActionButton,
+  DropdownMenu,
+  DropdownMenuItem,
+  Switch,
+  Text,
+  Tooltip,
+} from '@stone-payments/jade';
+import classNames from 'classnames';
 import { useState } from 'react';
 import { ContainerVariant } from '../../types';
 
@@ -18,8 +26,19 @@ export default function LayoutControls({
   const containerVariants: ContainerVariant[] = ['default', 'narrow', 'wide', 'full'];
   const [dropdownOpen, toggleDropdownOpen] = useState(false);
 
+  const wrapperClasses = classNames(
+    'group fixed bottom-0 z-50 flex w-full translate-y-full justify-center px-2 pb-2 md:translate-y-0 md:pb-5',
+    {
+      'md:w-[calc(100%_-_var(--app-sidebar-width-md))] lg:w-[calc(100%_-_var(--app-sidebar-width-lg))] left-auto right-0':
+        sidebarVisiblity,
+    },
+    {
+      'md:left-0 md:w-auto lg:left-0 lg:w-full right-auto left-0': !sidebarVisiblity,
+    }
+  );
+
   return (
-    <div className="group fixed bottom-0 z-50 flex w-full translate-y-full justify-center px-2 pb-2 transition-transform duration-200 ease-in-out md:left-(--app-sidebar-width-md) md:w-[calc(100%_-_var(--app-sidebar-width-md))] md:translate-y-0 md:pb-5 lg:left-(--app-sidebar-width-lg) lg:w-[calc(100%_-_var(--app-sidebar-width-lg))]">
+    <div className={wrapperClasses}>
       <div className="flex grow-0 basis-auto items-stretch justify-center gap-5 rounded-2xl border border-slate-200 bg-white/20 p-3 shadow-2xl backdrop-blur-lg transition-all duration-200 ease-in-out group-hover:translate-y-0 md:w-auto">
         <div className="flex items-center gap-2 pl-3">
           <Text
@@ -34,7 +53,7 @@ export default function LayoutControls({
           <Switch
             id="sidebar-visibility"
             name="sidebar-visibility"
-            onClick={() => onSidebarVisibilityChange(sidebarVisiblity ? false : true)}
+            onChange={() => onSidebarVisibilityChange(sidebarVisiblity ? false : true)}
             checked={sidebarVisiblity}
           />
         </div>
